c6e558ce6226f1967d2d90fd9e463ab7d3bd9ad0
unknown <tr9836859@gmail.com>
2021-09-22 22:12:59 +0600

    I commit double_vs_triple

---
+++ double_vs_triple.js
@@ -0,0 +1,28 @@
+// double shudu check kore man ta soman kina not type jemon eikhane firstName e string er moddeh ase 2 and secondNum e ase number type but man 2 both side e dekhei true print koreche;
+
+// otherside triple check kore man and type soho!:
+
+
+const firstNum = "2";
+const secondNum = 2;
+
+if(firstNum == secondNum){
+    console.log("true");
+}
+
+else{
+    console.log("false");
+}
+
+
+// jemon eikhane ami x ke string theke Number diye number e and y ke String diye number theke string e convert korechi! jehetu both x and y er type alada but man ek tao false print korbe
+
+const x = Number("10");
+const y = String(10);
+
+if(x===y){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
