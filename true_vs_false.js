ecbcf1fdd0692d2c132f36c159b3a4593f1c1bdd
unknown <tr9836859@gmail.com>
2021-09-22 21:24:09 +0600

    I commit this for advance js

---
+++ true_vs_false.js
@@ -0,0 +1,21 @@
+// false value:
+// age = 0
+// undefined(mane age variable e kono man nah dile)
+// ""
+// null,NaN;
+
+
+// truth value:
+// []," "
+
+
+
+let name = NaN;
+
+if(name){
+    console.log("This is truth");
+}
+
+else{
+    console.log("This is false");
+}
